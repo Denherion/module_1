2f8ce9e35b96b208314ddba3c1b4867fc453f874
Denherion <ydenisk@gmail.com>
2024-11-09 00:02:37 +1000

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,4 @@
+name = 'Python'
+homework_count = 12
+hours_count = 1.5
+print('Курс', ': ', (name), ', ', 'всего задач', ': ', (homework_count), ', ', 'затрачено часов', ': ', (hours_count), ', ', 'среднее время выполнения', ': ', hours_count / homework_count)
